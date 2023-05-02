1e8046ad974371194f7c87ef98fb1d7f2d7653e6
vasumamidi <130634589+vasumamidi@users.noreply.github.com>
2023-05-02 05:50:16 +0530

    one

---
+++ RecipeBackendProject-main/RecipeBackendProject-main/RecipeProject/src/main/java/com/recipes/entity/Ingredients.java
@@ -0,0 +1,70 @@
+package com.recipes.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "INGREDIENTS")
+public class Ingredients {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonIgnore
+	private int id;
+	
+	@Column
+	private String ingredientsList;
+	
+	
+	
+	public Ingredients() {
+		super();
+	}
+
+
+
+	public Ingredients(int id, String ingredientsList) {
+		super();
+		this.id = id;
+		this.ingredientsList = ingredientsList;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getIngredientsList() {
+		return ingredientsList;
+	}
+
+
+
+	public void setIngredientsList(String ingredientsList) {
+		this.ingredientsList = ingredientsList;
+	}
+
+
+
+
+
+
+	
+	
+}
---
+++ RecipeBackendProject-main/RecipeBackendProject-main/RecipeProject/src/main/java/com/recipes/exception/RecipeNotFoundException.java
@@ -0,0 +1,15 @@
+package com.recipes.exception;
+
+public class RecipeNotFoundException extends Exception{
+
+	public RecipeNotFoundException(String message) {
+		
+		super(message);
+		
+	}
+
+	
+	
+	
+
+}
